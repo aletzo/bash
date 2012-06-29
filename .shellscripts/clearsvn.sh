9d28a58488cb4c4c627d2aa80b50156b748751d7
aletzo <ajo@mydevnull.net>
2012-06-29 13:15:28 +0200

    changed the confirmation message

--- .shellscripts/clearsvn.sh
+++ .shellscripts/clearsvn.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-echo "recursively removing .svn folders from"
+echo "recursively removing .svn directories from"
 pwd
 rm -rf `find . -type d -name .svn`
+echo "The dude abides!"
--- .shellscripts/switchphpini.sh
+++ .shellscripts/switchphpini.sh
@@ -4,4 +4,4 @@ sudo cp /private/etc/php.ini /private/etc/php_prev_new.ini
 sudo cp /private/etc/php_prev.ini /private/etc/php.ini
 sudo cp /private/etc/php_prev_new.ini /private/etc/php_prev.ini
 sudo /usr/sbin/apachectl restart
-echo "Done dude!"
+echo "The dude abides!"
